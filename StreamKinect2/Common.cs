751c34f92241e587dc26dcd3e661ba068492bdb9
Rich Wareham <rjw57@cam.ac.uk>
2014-09-15 13:58:49 +0100

    Add missing files

---
+++ StreamKinect2/Common.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace StreamKinect2
+{
+    public enum MessageType : byte
+    {
+        ERROR = 0x00,
+        PING = 0x01,
+        PONG = 0x02,
+        WHO = 0x03,
+        ME = 0x04,
+
+        INVALID = 0xff,
+    }
+
+    public class Payload { }
+
+    public class ErrorPayload : Payload
+    {
+        public string reason;
+    }
+
+    public class DeviceRecord : Payload
+    {
+        public string id;
+        public IDictionary<string, string> endpoints;
+    }
+
+    public class MePayload : Payload
+    {
+        public int version;
+        public string name;
+        public IDictionary<string, string> endpoints;
+        public IList<DeviceRecord> devices;
+    }
+}
