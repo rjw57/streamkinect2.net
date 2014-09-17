e27c9d922627fc5a16dbad2d0daa92ca721f7452
Rich Wareham <rjw57@cam.ac.uk>
2014-09-17 22:36:16 +0100

    device: add depth frame callback support for simulated device

--- StreamKinect2/Device.cs
+++ StreamKinect2/Device.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Kinect;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace StreamKinect2
 {
@@ -12,11 +14,20 @@ namespace StreamKinect2
 
     public delegate void DepthFrameHandler(IDevice device, DepthFrameHandlerArgs args);
 
+    [Flags]
+    public enum DeviceOutputFlags
+    {
+        None    = 0x00,
+        Depth   = 0x01,
+    }
+
     public interface IDevice
     {
         event DepthFrameHandler DepthFrame;
 
         string UniqueId { get; }
+
+        DeviceOutputFlags ActiveOutputs { get; set; }
     }
 
     public class KinectDevice : IDevice
@@ -34,23 +45,107 @@ namespace StreamKinect2
         {
             get { return m_sensor.UniqueKinectId; }
         }
+
+        public DeviceOutputFlags ActiveOutputs
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 
-    public class SimulatedKinectDevice : IDevice
+    public class SimulatedKinectDevice : IDevice, IDisposable
     {
         public event DepthFrameHandler DepthFrame;
 
         private string m_uniqueId;
+        private Task m_depthFrameTask;
+        private bool m_depthFrameTaskShouldExit;
 
         public SimulatedKinectDevice()
         {
             // the unique ID is simply a GUID
             m_uniqueId = Guid.NewGuid().ToString();
+
+            m_depthFrameTask = null;
+            m_depthFrameTaskShouldExit = false;
         }
 
         public string UniqueId
         {
             get { return m_uniqueId; }
+        }
+
+        private void DepthThread()
+        {
+            var args = new DepthFrameHandlerArgs
+            {
+                Width = 1920, Height = 1080,
+                FrameData = new UInt16[1920 * 1080],
+            };
+
+            while (!m_depthFrameTaskShouldExit)
+            {
+                var then = System.DateTime.Now;
+                for (int y = 0; y < args.Height; y++)
+                    for (int x = 0; x < args.Width; x++)
+                    {
+                        args.FrameData[x + (y * args.Width)] = 100;
+                    }
+
+                DepthFrame(this, args);
+                var now = System.DateTime.Now;
+
+                var delta = now - then;
+                Thread.Sleep((int)(Math.Max(0, 1000 / 60 - delta.TotalMilliseconds)));
+            }
+        }
+
+        public DeviceOutputFlags ActiveOutputs
+        {
+            get
+            {
+                DeviceOutputFlags flags = DeviceOutputFlags.None;
+                if ((m_depthFrameTask != null) && !m_depthFrameTask.IsCanceled)
+                {
+                    flags |= DeviceOutputFlags.Depth;
+                }
+                return flags;
+            }
+            set
+            {
+                DeviceOutputFlags currentOutputs = ActiveOutputs;
+                DeviceOutputFlags changedOutputs = currentOutputs ^ value;
+
+                if ((changedOutputs & DeviceOutputFlags.Depth) != 0)
+                {
+                    // Either we need to stop or start depth output
+                    if ((value & DeviceOutputFlags.Depth) != 0)
+                    {
+                        // Start
+                        m_depthFrameTaskShouldExit = false;
+                        m_depthFrameTask = Task.Factory.StartNew(DepthThread);
+                    }
+                    else
+                    {
+                        // Stop
+                        m_depthFrameTaskShouldExit = true;
+                        m_depthFrameTask.Wait(300);
+                        m_depthFrameTask = null;
+                        m_depthFrameTaskShouldExit = false;
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            ActiveOutputs = DeviceOutputFlags.None;
         }
     }
 }
---
+++ StreamKinect2Tests/SimulatedKinectTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StreamKinect2;
+using System.Threading;
+using System.Diagnostics;
+
+namespace StreamKinect2Tests
+{
+
+    [TestClass]
+    public class SimulatedKinectTests
+    {
+        // Access to kinect device both through interface and actual type
+        private IDevice m_device;
+        private SimulatedKinectDevice m_simulatedDevice;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            m_simulatedDevice = new SimulatedKinectDevice();
+            m_device = m_simulatedDevice;
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            m_simulatedDevice.Dispose();
+            m_simulatedDevice = null;
+            m_device = null;
+        }
+        
+        [TestMethod]
+        public void DepthFrameProperty()
+        {
+            Assert.AreEqual(DeviceOutputFlags.None, m_device.ActiveOutputs);
+            m_device.ActiveOutputs |= DeviceOutputFlags.Depth;
+            Assert.AreEqual(DeviceOutputFlags.Depth, m_device.ActiveOutputs);
+            m_device.ActiveOutputs &= ~DeviceOutputFlags.Depth;
+            Assert.AreEqual(DeviceOutputFlags.None, m_device.ActiveOutputs);
+        }
+
+        [TestMethod, Timeout(3000)]
+        public void DepthFrameHandlerCalledALeastOnce()
+        {
+            int depthFrames = 0;
+            m_device.DepthFrame += (IDevice device, DepthFrameHandlerArgs args) => depthFrames += 1;
+            m_device.ActiveOutputs |= DeviceOutputFlags.Depth;
+            while (depthFrames == 0) { Thread.Sleep(100); }
+        }
+
+        [TestMethod, Timeout(3000)]
+        public void DepthFrameHandlerCalledEnough()
+        {
+            int depthFrames = 0;
+            m_device.DepthFrame += (IDevice device, DepthFrameHandlerArgs args) => depthFrames += 1;
+            m_device.ActiveOutputs |= DeviceOutputFlags.Depth;
+            Thread.Sleep(1000);
+            m_device.ActiveOutputs &= ~DeviceOutputFlags.Depth;
+
+            Debug.WriteLine("Waited one second and received " + depthFrames + " frames.");
+            
+            // Be generous in what we accept here
+            Assert.IsTrue(depthFrames > 30);
+        }
+
+        [TestMethod, Timeout(3000)]
+        public void DepthFrameIs1080p()
+        {
+            int depthFrames = 0, frameWidth = 0, frameHeight = 0;
+            m_device.DepthFrame += (IDevice device, DepthFrameHandlerArgs args) =>
+            {
+                depthFrames += 1;
+                frameWidth = args.Width;
+                frameHeight = args.Height;
+            };
+            m_device.ActiveOutputs |= DeviceOutputFlags.Depth;
+            while (depthFrames == 0) { Thread.Sleep(100); }
+            Assert.AreEqual(1920, frameWidth);
+            Assert.AreEqual(1080, frameHeight);
+        }
+
+        [TestMethod, Timeout(3000)]
+        public void DepthFrameIsOnly12Bit()
+        {
+            UInt16[] frameCopy = new UInt16[1920*1080];
+            int depthFrames = 0;
+            m_device.DepthFrame += (IDevice device, DepthFrameHandlerArgs args) =>
+            {
+                depthFrames += 1;
+                Assert.IsTrue(args.FrameData.Length <= frameCopy.Length);
+                args.FrameData.CopyTo(frameCopy, 0);
+            };
+            m_device.ActiveOutputs |= DeviceOutputFlags.Depth;
+            while (depthFrames == 0) { Thread.Sleep(100); }
+
+            foreach(UInt16 v in frameCopy)
+            {
+                Assert.IsTrue(v <= (1 << 12));
+            }
+        }
+
+    }
+}
