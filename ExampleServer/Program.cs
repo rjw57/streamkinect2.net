ccf52c516f29a1543e618c89123e7eea6694183e
Rich Wareham <rjw57@cam.ac.uk>
2014-09-19 11:05:07 +0100

    exampleserver: make robust to loading on non-Windows 8

--- ExampleServer/Program.cs
+++ ExampleServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using StreamKinect2;
 using System;
+using System.Diagnostics;
 
 namespace ExampleServer
 {
@@ -7,14 +8,23 @@ namespace ExampleServer
     {
         static void Main(string[] args)
         {
-            IDevice device;
+            IDevice device = null;
 
-            if(KinectDevice.DefaultDevice != null)
+            try
             {
-                Console.WriteLine("Hardware kinect was found.");
-                device = KinectDevice.DefaultDevice;
+                if (KinectDevice.DefaultDevice != null)
+                {
+                    Console.WriteLine("Hardware kinect was found.");
+                    device = KinectDevice.DefaultDevice;
+                }
             }
-            else
+            catch (TypeInitializationException)
+            {
+                // This is usually due to somehow running on Windows <8
+                Debug.WriteLine("Failed to load Kinect driver.");
+            }
+
+            if (device == null)
             {
                 Console.WriteLine("No hardware kinect found, using simulated device.");
                 device = new SimulatedDevice();
