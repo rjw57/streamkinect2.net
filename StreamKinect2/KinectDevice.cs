01ff22f820ad73adef37527973561afab006fc32
Rich Wareham <rjw57@cam.ac.uk>
2014-10-14 16:44:04 +0100

    KinectDevice: add internal depth frame source

--- StreamKinect2/KinectDevice.cs
+++ StreamKinect2/KinectDevice.cs
@@ -8,14 +8,59 @@ using System.Threading.Tasks;
 
 namespace StreamKinect2
 {
+    internal class KinectDeviceDepthFrameSource : IDepthFrameSource
+    {
+        public event DepthFrameHandler DepthFrame;
+        private DepthFrameReader depthFrameReader;
+        private bool isRunning;
+
+        public KinectDeviceDepthFrameSource(DepthFrameReader depthFrameReader)
+        {
+            this.depthFrameReader = depthFrameReader;
+            this.depthFrameReader.FrameArrived += depthFrameReader_FrameArrived;
+            this.isRunning = false;
+        }
+
+        private void depthFrameReader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        {
+            // TODO: handle depth frame
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+        }
+
+        public void Start()
+        {
+            this.isRunning = true;
+        }
+
+        public void Stop()
+        {
+            this.isRunning = false;
+        }
+
+        public void Dispose()
+        {
+            // NOP
+        }
+    }
+
     public class KinectDevice : IDevice
     {
         public static KinectDevice DefaultDevice = CreateDefaultDevice();
+
         private KinectSensor sensor;
+        private DepthFrameReader depthFrameReader;
+        private KinectDeviceDepthFrameSource depthFrameSource;
 
         private KinectDevice(KinectSensor sensor)
         {
             this.sensor = sensor;
+            this.depthFrameReader = this.sensor.DepthFrameSource.OpenReader();
+            this.depthFrameSource = new KinectDeviceDepthFrameSource(this.depthFrameReader);
+            this.sensor.Open();
         }
 
         public string UniqueId
@@ -25,7 +70,7 @@ namespace StreamKinect2
 
         public IDepthFrameSource DepthFrameSource
         {
-            get { throw new NotImplementedException(); }
+            get { return this.depthFrameSource; }
         }
 
         private static KinectDevice CreateDefaultDevice()
