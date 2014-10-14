0e10a44d7bc84b1844212e2c23059312a3bf859c
Rich Wareham <rjw57@cam.ac.uk>
2014-10-14 16:50:27 +0100

    KinectDevice: start work on wiring up source

--- StreamKinect2/KinectDevice.cs
+++ StreamKinect2/KinectDevice.cs
@@ -11,19 +11,62 @@ namespace StreamKinect2
     internal class KinectDeviceDepthFrameSource : IDepthFrameSource
     {
         public event DepthFrameHandler DepthFrame;
+
         private DepthFrameReader depthFrameReader;
         private bool isRunning;
+        private FrameDescription depthFrameDescription;
+        private DepthFrameHandlerArgs depthFrameArgs;
 
         public KinectDeviceDepthFrameSource(DepthFrameReader depthFrameReader)
         {
             this.depthFrameReader = depthFrameReader;
+            this.depthFrameDescription = depthFrameReader.DepthFrameSource.FrameDescription;
             this.depthFrameReader.FrameArrived += depthFrameReader_FrameArrived;
             this.isRunning = false;
+
+            this.depthFrameArgs = new DepthFrameHandlerArgs
+            {
+                Width = this.depthFrameDescription.Width,
+                Height = this.depthFrameDescription.Height,
+                FrameData = new UInt16[this.depthFrameDescription.Width * this.depthFrameDescription.Height],
+            };
         }
 
         private void depthFrameReader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
         {
-            // TODO: handle depth frame
+            using (DepthFrame depthFrame = e.FrameReference.AcquireFrame())
+            {
+                if (depthFrame != null)
+                {
+                    // the fastest way to process the body index data is to directly access 
+                    // the underlying buffer
+                    using (Microsoft.Kinect.KinectBuffer depthBuffer = depthFrame.LockImageBuffer())
+                    {
+                        // verify data and write the color data to the display bitmap
+                        if ((this.depthFrameDescription.Width * this.depthFrameDescription.Height) == (depthBuffer.Size / this.depthFrameDescription.BytesPerPixel))
+                        {
+                            // Note: In order to see the full range of depth (including the less reliable far field depth)
+                            // we are setting maxDepth to the extreme potential depth threshold
+                            ushort maxDepth = ushort.MaxValue;
+
+                            // If you wish to filter by reliable depth distance, uncomment the following line:
+                            //// maxDepth = depthFrame.DepthMaxReliableDistance
+
+                            this.ProcessDepthFrameData(depthBuffer.UnderlyingBuffer, depthBuffer.Size, depthFrame.DepthMinReliableDistance, maxDepth);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ProcessDepthFrameData(IntPtr depthFrameData, uint depthFrameDataSize, ushort minDepth, ushort maxDepth)
+        {
+            // depth frame data is a 16 bit value
+            //ushort* frameData = (ushort*)depthFrameData;
+
+            if (DepthFrame != null) {
+                DepthFrame(this, depthFrameArgs);
+            }
         }
 
         public bool IsRunning
